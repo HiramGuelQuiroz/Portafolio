ce026cccbc46bd07d17849f6457fad38d1018ffc
Hiram Guel <111021774+HiramGuelQuiroz@users.noreply.github.com>
2024-03-31 10:14:25 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import { RouterPrincipal } from './router/RouterPrincipal';
+
+function App() {
+  return (
+   <div className='layout'>
+      <RouterPrincipal/>
+   </div>
+  );
+}
+
+export default App;
